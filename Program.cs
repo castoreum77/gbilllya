ff5cb427aec27062fd07c1c87ccf83e0d61b9edf
castoreum77 <114386065+castoreum77@users.noreply.github.com>
2022-10-27 17:28:49 +0300

    задачи 47 и 50

--- Program.cs
+++ Program.cs
@@ -1,29 +1,100 @@
-﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.Clear();
-Console.Write("Введите число ");
-int m = Convert.ToInt32(Console.ReadLine());
-int result = 0;
-for (int i = 1; i <= m; i++)
+void InputMatrix(double[,] matrix)
 {
-    int n = Convert.ToInt32(Console.ReadLine());
-    if (n > 0)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        result = result + 1;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = new Random().NextDouble() * 100;
     }
 }
-Console.Write($"всего {result} чисел больше нуля");
-*/
+
+
+void PrintMatrix(double[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {       
+            for (int j = 0; j < matrix.GetLength(1); j++)
+        Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
 
 Console.Clear();
-Console.Write("Введите k1: ");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите k2: ");
-double k2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b1: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b2: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-double x = (b1 - b2) / (k2 - k1);
-double y = ((k2 * x) + b2);
-Console.Write($"({x}, {y})");
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+*/
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+void PrintXY(int[,] matrix)
+{
+    Console.Write("Введите x: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите y: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(matrix[x-1,y-1]);
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintXY(matrix);
+*/
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+
+int sum = 0;
+double sr = 0;
+for (int j = 0; j <= m-1; j++)
+{
+    for (int i = 0; i <= n-1; i++)
+    {
+        sum = sum + matrix[i,j];
+    }
+sr = Convert.ToDouble(sum) / n; 
+Console.WriteLine(sr);
+sum = 0;
+}
