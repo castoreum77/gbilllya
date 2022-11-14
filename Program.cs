faa93bec352cb3aac136c07beeee6ed5afe5956a
castoreum77 <114386065+castoreum77@users.noreply.github.com>
2022-11-14 23:15:26 +0300

    семинар 9

--- Program.cs
+++ Program.cs
@@ -1,227 +1,41 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-void InputMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(-9, 10);
-            Console.Write($"{matrix[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.(НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
 
-void ReleaseMatrix(int[,] matrix)
+/*
+int Summa(int n, int m)
 {
-    for (int i=0; i<matrix.GetLength(0); i++)
-    {
-        for (int j=0; j<matrix.GetLength(1); j++)
-        {
-            for (int l=0; l<matrix.GetLength(1)-1; l++)
-        
-            if (matrix[i, l]>matrix[i, l+1])
-            {
-                int temp = matrix[i, l+1];
-                matrix[i, l+1] = matrix[i, l];
-                matrix[i, l] = temp; 
-            }
-        }
-    }
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
+    if (n <= m - 1)
+        return Summa(n + 1, m) + n;
+    else return n * 2;
 }
 
 Console.Clear();
-Console.Write("Введите кол-во строк: ");
+Console.Write("Введите число 1-ое: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
+Console.Write("Введите число 2-ое: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[n, m];
-Console.WriteLine("Begin: ");
-InputMatrix(matrix);
-Console.WriteLine("Result: ");
-ReleaseMatrix(matrix);
+Console.WriteLine(Summa(n, m) - m);
 */
 
-/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-void InputMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(-9, 10);
-            Console.Write($"{matrix[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
-int SummaStroki(int[,] matrix, int i)
-{
-    int Sum = matrix[i, 0];
-    for (int j = 1; j < matrix.GetLength(1); j++)
-    {
-        Sum += matrix[i,j];
-    }
-    return Sum;
-}
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-
+/*
+int Akkerman(int n, int m)
+{
+	if (n == 0)
+	{
+		return m + 1;
+	}
+	if (n > 0 && m == 0)
+	{
+		return Akkerman(n - 1, 1);
+	}
+	return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+		
 Console.Clear();
-Console.Write("Введите кол-во строк: ");
+Console.Write("Введите число 1-ое: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
+Console.Write("Введите число 2-ое: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[n, m];
-Console.WriteLine("Begin: ");
-InputMatrix(matrix);
-int MinIndex = 0;
-int SumLine = SummaStroki(matrix, 0);
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    int temp = SummaStroki(matrix, i);
-    if (SumLine > temp)
-    {
-        SumLine = temp;
-        MinIndex = i;
-    }
-}
-Console.WriteLine("Result: ");
-Console.WriteLine(MinIndex);
+Console.WriteLine(Akkerman(n, m));
 */
-/*Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-void InputMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(-9, 10);
-            Console.Write($"{matrix[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
-
-void WriteArray (int[,] matrix)
-{
-  for (int i = 0; i < matrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-      Console.Write(matrix[i,j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
-
-Console.Clear();
-Console.Write("Введите кол-во строк: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrix1 = new int[n, m];
-int[,] matrix2 = new int[n, m];
-int[,] matrix3 = new int[n, m];
-Console.WriteLine("первая Матрица: ");
-InputMatrix(matrix1);
-Console.WriteLine("Вторая Матрица: ");
-InputMatrix(matrix2);
-void ReleaseMatrix(int[,] matrix1, int[,]matrix2, int[,] matrix3)
-{
-    for (int i = 0; i < matrix3.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix3.GetLength(1); j++)
-        {
-                int sum = 0;
-                for (int k = 0; k < matrix1.GetLength(1); k++)
-                {
-                    sum += matrix1[i, k] * matrix2[k, j];
-                }
-                matrix3[i, j] = sum;
-        }
-    }    
-}
-ReleaseMatrix(matrix1, matrix2, matrix3);
-Console.WriteLine("произведение матриц: ");
-WriteArray(matrix3);
-*/
-/*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-void InputMatrix(int[,,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int n = 0; n < matrix.GetLength(2); n++)
-                matrix[i, j, n] = new Random().Next(-9, 10);
-        }
-    }
-}
-
-void PrintArray(int[,,]matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int n = 0; n < matrix.GetLength(2); n++)
-            Console.WriteLine(matrix[i, j, n] + " (" + i + ", " + j + ", " + n + ")");
-        }
-    }
-}
-
-Console.Clear();
-Console.Write("Введите x: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y: ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите z: ");
-int z = Convert.ToInt32(Console.ReadLine());
-int[,,] matrix = new int[x, y, z];
-Console.WriteLine("Result: ");
-InputMatrix(matrix);
-PrintArray(matrix);
-*/
-
-void InputMatrix(int[,] matrix)
-{
-    int temp = 1; 
-    int i = 0; 
-    int j = 0;
-    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
-    {
-        matrix[i, j] = temp;
-        temp++;
-        if (i <= j && i + j < matrix.GetLength(1) - 1)
-            j++;
-        else if (i < j && i + j >= matrix.GetLength(0) - 1)
-            i++;
-        else if (i >= j && i + j > matrix.GetLength(1) - 1)
-            j--;
-        else 
-            i--;
-    }
-    for (i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (j = 0; j < matrix.GetLength(1); j++)
-            Console.Write(matrix[i, j] + " \t");
-        Console.WriteLine();
-    }
-}
-
-Console.Clear();
-Console.Write("Введите кол-во строк: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[n, m];
-Console.WriteLine("Matrix: ");
-InputMatrix(matrix);
