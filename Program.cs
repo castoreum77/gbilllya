97dc9a604bd718d78bff6ca69ae1fe96d8f79907
castoreum77 <114386065+castoreum77@users.noreply.github.com>
2022-11-24 12:32:21 +0300

    создал массив который заполняет массив 3 на 3 рандомнцыми числами

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = new Random().Next(-9, 10);
+        }
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
@@ -13,3 +22,6 @@
 
 Console.Clear();
 int[,] matrix = new int[3, 3];
+Console.WriteLine("Begin: ");
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
