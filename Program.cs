d5ccfc7a2caf13a0937ae920052729a9eda4cc1b
castoreum77 <114386065+castoreum77@users.noreply.github.com>
2022-11-24 12:21:20 +0300

    добавил заполнение массива через войд

--- Program.cs
+++ Program.cs
@@ -1,41 +1,12 @@
-﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.(НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
-
-/*
-int Summa(int n, int m)
+﻿void InputMatrix(int[,] matrix)
 {
-    if (n <= m - 1)
-        return Summa(n + 1, m) + n;
-    else return n * 2;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
 }
-
-Console.Clear();
-Console.Write("Введите число 1-ое: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число 2-ое: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Summa(n, m) - m);
-*/
-
-//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-
-/*
-int Akkerman(int n, int m)
-{
-	if (n == 0)
-	{
-		return m + 1;
-	}
-	if (n > 0 && m == 0)
-	{
-		return Akkerman(n - 1, 1);
-	}
-	return Akkerman(n - 1, Akkerman(n, m - 1));
-}
-		
-Console.Clear();
-Console.Write("Введите число 1-ое: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число 2-ое: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Akkerman(n, m));
-*/
