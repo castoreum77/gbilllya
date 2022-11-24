3b26965e969973c6738bc520c4eb80d2d1f958ca
castoreum77 <114386065+castoreum77@users.noreply.github.com>
2022-11-24 14:02:37 +0300

    добавил методы для обработки и вывода массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿Console.Clear();
-string[] matrix = new string[9] {"Hello", "2", "world", "1234", "1567", "-2", "Russia", "Tatarstan", "computert science"};
+﻿void ArrayProcessing(string[] OriginalArray, string[] NewArray)
+{
+    int count = 0;
+    for (int i = 0; i < OriginalArray.GetLength(0); i++)
+    {
+        if (OriginalArray[i].Length <= 3)
+        {
+            NewArray[count] = OriginalArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] NewArray)
+{
+    for (int i = 0; i < NewArray.GetLength(0); i++)
+    {
+        Console.Write($"{NewArray[i]}, ");
+    }
+}
+Console.Clear();
+string[] OriginalArray = new string[9] {"Hello", "2", "world", "1234", "1567", "-2", "Russia", "Tatarstan", "computert science"};
+string[] NewArray = new string[9];
